4a9995cda5b6426345561b6487ad70cee56e7573
Diana <dianapo0108@gmail.com>
2025-02-23 18:56:33 +0100

    import simpleLightBox

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,4 +1,5 @@
 import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 export function showLoader(loader) {
     loader.classList.remove('hidden');
@@ -19,14 +20,16 @@ export function displayImages(images) {
 
         listItem.innerHTML = `
         <a href="${image.largeImageURL}" class="gallery-link">
-                <img src="${image.webformatURL}" alt="${image.tags}" class="gallery-image" />
-            </a>
+            <img src="${image.webformatURL}" alt="${image.tags}" class="gallery-image" />
+        </a>
         <div class="info">
             <p>Likes <span>${image.likes}</span></p>
             <p>Views <span>${image.views}</span></p>
             <p>Comments <span>${image.comments}</span></p>
             <p>Downloads <span>${image.downloads}</span></p>
-        </div>`;
+        </div>
+    `;
+    
 
         gallery.appendChild(listItem);
     });
@@ -34,6 +37,5 @@ export function displayImages(images) {
     const lightbox = new SimpleLightbox('.gallery-link', {
         captionsData: "alt"
     });
-
     lightbox.refresh();
 }
